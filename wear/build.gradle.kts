0eab43ffe9a173041a3e413a1ab28f5778b8fb4c
Siddharth Pandalai <siddharthpandalai990@gmail.com>
2026-02-04 09:32:44 +0530

    build(ci): add GitHub Actions quality workflow and Wear OS tile scaffold

---
+++ wear/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+}
+
+android {
+    namespace = "com.miletracker.wear"
+    compileSdk = 36
+    defaultConfig {
+        minSdk = 30
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.wear.tiles)
+    implementation(libs.wear.tiles.material)
+    implementation(libs.core.ktx)
+    implementation("com.google.guava:guava:33.4.0-android")
+}
