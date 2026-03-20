6a298c7b210c412d2c8913e20cae17c6459cf7e5
Siddharth Pandalai <siddharthpandalai990@gmail.com>
2026-03-20 14:12:04 +0530

    build(deps): migrate to JetBrains multiplatform Navigation Compose 2.9.2

--- feature/tracking/build.gradle.kts
+++ feature/tracking/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 
     implementation(libs.core.ktx)
     implementation(libs.activity.compose)
-    implementation(libs.navigation.compose)
+    implementation(libs.jb.navigation.compose)
     implementation(libs.lifecycle.viewmodel)
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
