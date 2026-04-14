9b14df811ddb54582e646276bb8554023a79e144
Siddharth Pandalai <siddharthpandalai990@gmail.com>
2026-04-14 17:48:28 +0530

    build(plugins): add kmp.compose convention plugin and migrate core KMP modules

---
+++ build-logic/convention/src/main/kotlin/MileTrackerKmpComposeConventionPlugin.kt
@@ -0,0 +1,19 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Convention plugin for a Kotlin Multiplatform **Compose** library targeting Android + iOS.
+ *
+ * Builds on [MileTrackerKmpLibraryConventionPlugin] (Kotlin MPP + AGP KMP-library + iOS targets) and adds
+ * the Compose Multiplatform + Compose compiler plugins. Consuming modules declare only their
+ * `android { namespace/compileSdk/minSdk }` block + source sets.
+ */
+class MileTrackerKmpComposeConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) = with(target) {
+        with(pluginManager) {
+            apply("miletracker.kmp.library")
+            apply("org.jetbrains.compose")
+            apply("org.jetbrains.kotlin.plugin.compose")
+        }
+    }
+}
