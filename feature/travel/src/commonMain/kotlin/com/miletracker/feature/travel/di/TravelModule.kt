fa2706c9b747e2c5666f7a5fa5152fd3265b9303
Siddharth Pandalai <siddharthpandalai990@gmail.com>
2026-05-14 16:10:06 +0530

    feat(kmp): migrate :feature:travel to Compose Multiplatform

---
+++ feature/travel/src/commonMain/kotlin/com/miletracker/feature/travel/di/TravelModule.kt
@@ -0,0 +1,8 @@
+package com.miletracker.feature.travel.di
+
+import com.miletracker.feature.travel.repository.TravelRepository
+import org.koin.dsl.module
+
+val travelModule = module {
+    single { TravelRepository() }
+}
