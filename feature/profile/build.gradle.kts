14af63a47acb74fdb02c224323744bb465788a62
Siddharth Pandalai <siddharthpandalai990@gmail.com>
2026-05-31 09:14:02 +0530

    build(plugins): add cmp.feature convention plugin for Compose Multiplatform features

--- feature/profile/build.gradle.kts
+++ feature/profile/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("miletracker.kmp.compose")
+    id("miletracker.cmp.feature")
 }
 
 kotlin {
@@ -11,20 +11,6 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            implementation(libs.runtime)
-            implementation(libs.ui)
-            implementation(libs.material3)
-            implementation(libs.foundation)
-            implementation(libs.material.icons.extended)
-            implementation(libs.ui.tooling.preview.mp)
-
-            implementation(libs.koin.core)
-            implementation(libs.koin.compose)
-            implementation(libs.koin.compose.viewmodel)
-            implementation(libs.lifecycle.viewmodel)
-            implementation(libs.jb.navigation.compose)
-            implementation(libs.kotlinx.datetime)
-
             implementation(project(":core:common"))
             implementation(project(":core:ui"))
             implementation(project(":core:data"))
@@ -32,12 +18,6 @@ kotlin {
             implementation(project(":core:security"))
         }
         androidMain.dependencies {
-            implementation(libs.core.ktx)
-            implementation(libs.activity.compose)
-            implementation(libs.lifecycle.viewmodel.compose)
-            implementation(libs.lifecycle.runtime.compose)
-            implementation(libs.koin.android)
-            implementation(libs.kotlinx.coroutines.android)
             implementation("androidx.appcompat:appcompat:1.7.0")
             implementation(project(":stub"))
         }
